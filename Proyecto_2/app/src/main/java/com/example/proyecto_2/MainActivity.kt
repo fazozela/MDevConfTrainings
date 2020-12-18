95f72f69dbae89154099d8632089bf77a63df32d
Felix Alejandro Zelaya Orellana <fazogato@gmail.com>
2020-12-18 12:43:18 -0400

    Second Training

---
+++ Proyecto_2/app/src/main/java/com/example/proyecto_2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.proyecto_2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnLogin.setOnClickListener {
+            var intent = Intent(this,  MainContent::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
---
+++ Proyecto_2/app/src/main/java/com/example/proyecto_2/MainContent.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_content)
+    }
+}
